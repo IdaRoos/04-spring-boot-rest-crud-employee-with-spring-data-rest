9add6ca57e4cf6c38d37e44219c62729fd219743
IdaRoos <ida_r@live.se>
2023-06-07 18:12:45 +0200

    Init commit

--- src/main/java/com/idaroos/cruddemo/dao/EmployeeRepository.java
+++ src/main/java/com/idaroos/cruddemo/dao/EmployeeRepository.java
@@ -1,2 +1,12 @@
-package com.idaroos.cruddemo.dao;public interface EmployeeRepository {
+package com.idaroos.cruddemo.dao;
+
+import com.idaroos.cruddemo.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+// Custom url path
+//@RepositoryRestResource(path ="members")
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+    // that's it ... no need to write any code LOL!
 }
